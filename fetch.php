6db9007ed7a455ef0c339fe96381134ae31769de
George <isitgeorge@gmail.com>
2013-09-24 15:42:08 +0100

    Update files

---
+++ fetch.php
@@ -0,0 +1,74 @@
+<?php
+
+// Configuration
+
+$consumer_key = '';                               //
+$consumer_secret = '';           //
+                                                                       // These keys are available at https://dev.twitter.com/apps
+$access_token = '';  //
+$access_token_secret = '';  //
+
+$twitter_name = ''; // Your Twitter name
+$tweet_count = '1'; // The amount of tweets generate
+
+// End of configuration
+
+$oauth_array = array(
+'count' => $tweet_count,
+'oauth_consumer_key' => $consumer_key,
+'oauth_nonce' => time(),
+'oauth_signature_method' => 'HMAC-SHA1',
+'oauth_timestamp' => time(),
+'oauth_token' => $access_token,
+'oauth_version' => '1.0',
+'screen_name' => $twitter_name);
+
+$oauth_header = array();
+
+foreach ($oauth_array as $key => $value) {
+	array_push($oauth_header, $key . '=' . $value);
+}
+
+$oauth_header = implode('&', $oauth_header);
+$oauth_header = rawurlencode($oauth_header);
+
+$signature_base = 'GET&' . rawurlencode('https://api.twitter.com/1.1/statuses/user_timeline.json') . '&' . $oauth_header;
+$signature_keys = rawurlencode($consumer_secret) . '&' . rawurlencode($access_token_secret);
+$oauth_signature = base64_encode(hash_hmac('sha1', $signature_base, $signature_keys, true));
+$oauth_signature = rawurlencode($oauth_signature);
+
+$oauth_array['oauth_signature'] = $oauth_signature;
+ksort($oauth_array);
+
+$oauth_header = array();
+
+foreach ($oauth_array as $key => $value) {
+	array_push($oauth_header, $key . '=' . $value);
+}
+
+$oauth_header = implode(',', $oauth_header);
+
+// cURL request to Twitter
+
+$oauth_send = curl_init();
+
+curl_setopt($oauth_send, CURLOPT_HTTPHEADER, array('Authorization: OAuth ' . $oauth_header)); // OAuth header
+curl_setopt($oauth_send, CURLOPT_URL, 'https://api.twitter.com/1.1/statuses/user_timeline.json?count=' . $tweet_count . '&screen_name=' . $twitter_name); // URL to request
+curl_setopt($oauth_send, CURLOPT_HEADER, false); // Don't return header
+curl_setopt($oauth_send, CURLOPT_RETURNTRANSFER, true); // Return as string
+curl_setopt($oauth_send, CURLOPT_SSL_VERIFYPEER, false); // Don't verify
+
+$returned_string = curl_exec($oauth_send);
+curl_close($oauth_send);
+
+$latest_tweet = json_decode($returned_string, true);
+
+function returnTweet($number) {
+	$number = $number - 1;
+	global $latest_tweet;
+	return $latest_tweet[$number]['text'];
+}
+
+//Then use: echo returnTweet('1');
+
+?>
---
+++ index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+<head>
+<title>My latest Tweet!</title>
+</head>
+
+<body>
+<h1>My page!</h1>
+
+<?php 
+include 'twitter_status.php';
+
+echo 'My lastest Tweet: ';
+echo returnTweet('1'); // Display Tweet 1
+?>
+
+</body>
+</html>
+
